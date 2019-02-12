9fa6c8dae04b55f3064f145dc99cfec07558127a
Kirill Kolisnichenko <kolisnichenko.kir@gmail.com>
2019-02-12 16:04:03 +0200

    LoginController fixed

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -11,36 +11,41 @@ class LoginController extends Controller
 {
     public function login(Request $request)
     {
-        if (Auth::attempt([
-                'username' => $request->username, 
-                'password' => $request->password
-            ])) {
-            return redirect()->route('index');
-        } else {
+        $auth = Auth::attempt([
+            'username' => $request->username, 
+            'password' => $request->password]);
+
+        if (! $auth) {
             $user = User::where('username', $request->username)->first();
             
             if ($user === null) {
-                $data = $request->validate([
-                    'username' => ['required', 'string', 'max:100', 'unique:users'],
-                    'password' => ['required', 'string', 'min:6', 'max:100'],
-                ]);
-        
-                $user = User::create([
-                    'username' => $data['username'],
-                    'password' => Hash::make($data['password']),
-                ]);
-        
-                Auth::login($user);
-                return redirect()->route('index');
+                $this->register($request);
             } else {
                 return redirect()->route('index')->withInput();
             }
         }
+
+        return redirect()->route('index');
     }
 
     public function logout()
     {
         Auth::logout();
         return redirect()->route('index');
+    }
+
+    private function register($request)
+    {
+        $data = $request->validate([
+            'username' => ['required', 'string', 'max:100', 'unique:users'],
+            'password' => ['required', 'string', 'min:6', 'max:100'],
+        ]);
+
+        $user = User::create([
+            'username' => $data['username'],
+            'password' => Hash::make($data['password']),
+        ]);
+
+        Auth::login($user);
     }
 }
