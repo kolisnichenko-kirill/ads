a1e56ff2c23517498a9ac204acb447aa1cdcbe5f
Kirill Kolisnichenko <kolisnichenko.kir@gmail.com>
2019-02-12 14:37:26 +0200

    LoginController fixed

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -2,44 +2,28 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
-use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
-use App\Http\Requests\UserPost;
 use App\User;
 use Auth;
 
 class LoginController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Login Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles authenticating users for the application and
-    | redirecting them to your home screen. The controller uses a trait
-    | to conveniently provide its functionality to your applications.
-    |
-    */
-
-    use AuthenticatesUsers;
-
-    /**
-     * Where to redirect users after login.
-     *
-     * @var string
-     */
-    protected $redirectTo = '/';
-    
-
-    public function __construct(UserPost $request)
+    public function login(Request $request)
     {
-        if ($request->username !== null) {
+        if (Auth::attempt([
+                'username' => $request->username, 
+                'password' => $request->password
+            ])) {
+            return redirect()->route('index');
+        } else {
             $user = User::where('username', $request->username)->first();
             
             if ($user === null) {
-                $data = $request->validated();
+                $data = $request->validate([
+                    'username' => ['required', 'string', 'max:100', 'unique:users'],
+                    'password' => ['required', 'string', 'min:6', 'max:100'],
+                ]);
         
                 $user = User::create([
                     'username' => $data['username'],
@@ -47,12 +31,16 @@ class LoginController extends Controller
                 ]);
         
                 Auth::login($user);
+                return redirect()->route('index');
+            } else {
+                return redirect()->route('index')->withInput();
             }
         }
     }
 
-    public function username()
+    public function logout()
     {
-        return 'username';
+        Auth::logout();
+        return redirect()->route('index');
     }
 }
